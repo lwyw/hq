7631ab4f7fd8cfb660e96d05437db2a904ffc0b3
weeyoung <weeyounglim@gmail.com>
2015-03-07 17:18:41 +0800

    update readme

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # hq
 ## Dependencies
-<a href="http://www.mongodb.org/">MongoDB</a> running with default settings
+<p>~~<a href="http://www.mongodb.org/">MongoDB</a> running with default settings~~</p>
+<p>Using NeDB as database store instead. No non-npm dependencies!</p>
 ## Start
 npm start
 ## Test
--- config/nedb.js
+++ config/nedb.js
@@ -5,7 +5,10 @@ var Datastore = require('nedb'), winston = require('winston');
 exports.config = function () {
   var db = new Datastore({ filename: __dirname + '/../payments.db' });
   db.loadDatabase(function (err) {
-    if (err) { winston.error('Error loading NeDB', {error: err}); }
+    if (err) {
+      console.log(err);
+      winston.error('Error loading NeDB', {error: err});
+    }
   });
 
   return db;
